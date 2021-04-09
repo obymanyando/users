e3fae6a339b41203679db4db2dc0c0d7611cb607
Oby Manyando <33107568+obymanyando@users.noreply.github.com>
2021-04-09 15:09:08 +0300

    add back to home button

--- src/FullCard.js
+++ src/FullCard.js
@@ -6,25 +6,31 @@ const FullCard = ({ users }) => {
 	const { id } = useParams()
 	return (
 		<div className='fullCard'>
-			<h2>User details</h2>
-			<Button variant='info' href='/' className='btn'>
+			{/* <Button variant='info' href='/' className='btn'>
 				⇦
-			</Button>
-			<Container>
+			</Button> */}
+			<div className='user-details'>
+				<h2>User details</h2>
+				{/* <Button variant='info' href='/' className='btn'>
+					⇦
+				</Button> */}
+			</div>
+
+			<Container className='d-flex flex-column align-items-center w-50'>
 				{users
 					.filter((user) => user.id == id)
 					.map((user) => (
-						<Card className='h-100 align-center m-3' key={user.id}>
+						<Card className='d-flex flex-column h-100 w-50 m-3' key={user.id}>
 							<Card.Body className='d-flex flex-column align-items-center'>
-								<div className='d-flex flex-column mb-4 align-items-center'>
-									<Card.Title>Name: {user.name}</Card.Title>
-									<Card.Text>Username: @{user.username}</Card.Text>
-									<Card.Text>Website: {user.email}</Card.Text>
-									<Card.Text>Phone: {user.phone}</Card.Text>
-									<Card.Text>Copmany: {user.company.name}</Card.Text>
-									<Card.Text>Website: {user.website}</Card.Text>
+								<div className='d-flex flex-column mb-4'>
+									<Card.Title>- Name: {user.name}</Card.Title>
+									<Card.Text>- Username: @{user.username}</Card.Text>
+									<Card.Text>- Website: {user.email}</Card.Text>
+									<Card.Text>- Phone: {user.phone}</Card.Text>
+									<Card.Text>- Copmany: {user.company.name}</Card.Text>
+									<Card.Text>- Website: {user.website}</Card.Text>
 									<Card.Text>
-										Address:{' '}
+										- Address:{' '}
 										<ul>
 											<li>street: {user.address.street}</li>
 											<li>suite: {user.address.suite}</li>
@@ -37,6 +43,9 @@ const FullCard = ({ users }) => {
 						</Card>
 					))}
 			</Container>
+			<Button variant='info' href='/' className='btn'>
+				⇦
+			</Button>
 		</div>
 	)
 }
